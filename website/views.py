1d56a27ce1ceaebf964a7ac0e88316f2b37e4892
Nicholas <phamn1000@gmail.com>
2023-07-25 16:13:41 -0400

    Import render_templates to allow the home route to render the template 'home.html' for the home page. (NAV BAR)

--- website/views.py
+++ website/views.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>HOME</h1>"
+    return render_template('home.html')
 
 @views.route('/info')
 def info():
