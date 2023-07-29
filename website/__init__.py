06414c65810c1899fb5e072e0fd0aa5a7947b2bc
Nicholas <phamn1000@gmail.com>
2023-07-29 01:39:44 -0400

    Fixed Typo.

--- website/__init__.py
+++ website/__init__.py
@@ -7,7 +7,7 @@ def create_app():
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prfix='/')
-    app.register_blueprint(auth, url_prfix='/')
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     return app
