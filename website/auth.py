0a4277910c123826a9f123cc642f4e4a2f93f91b
Nicholas <phamn1000@gmail.com>
2023-08-01 16:00:58 -0400

    From post div file from html, added to register function to get form data and created simple requirements.

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    data = request.form
+    print(data)
+    return render_template("login.html", boolean=True)
 
 @auth.route("/logout")
 def logout():
     return "<p>LOGOUT</p>"
 
-@auth.route("/register")
+@auth.route("/register", methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password = request.form.get('password1')
+        password = request.form.get('password2')
+
     return render_template("register.html")
