61b30eac96cb07b869eb90021dc1aa570afc7d4c
Nicholas <phamn1000@gmail.com>
2023-08-02 15:48:30 -0400

    Set variables to request forms for all fields/forms.

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 views = Blueprint('views', __name__)
 
@@ -10,6 +10,16 @@ def home():
 def info():
     return render_template('info.html')
 
-@views.route('/analysis')
+@views.route('/analysis', methods=['GET', 'POST'])
 def analysis():
+    if request.method == 'POST':
+        symbol = request.form.get('symbol')
+        screener = request.form.get('screener')
+        exchange = request.form.get('exchange')
+        interval = request.form.get('interval')
+
+        print(symbol)
+        print(screener)
+        print(exchange)
+        print(interval)
     return render_template('analysis.html')
