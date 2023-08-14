44126c00b972214e2e0cea4e45e4cb695a260d31
Nicholas <phamn1000@gmail.com>
2023-08-14 19:37:34 -0400

    Created Database on run.

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy()
 DB_NAME = 'database.db'
@@ -15,5 +16,11 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Note
+
+    with app.app_context():
+        db.create_all()
+        print('Created Database')
 
     return app
