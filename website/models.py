01884be4126f1a04695203ada8e2e3a1c85930cd
Nicholas <phamn1000@gmail.com>
2023-08-14 19:25:44 -0400

    Interlocking notes to the user class to keep notes consisted to specified user.

--- website/models.py
+++ website/models.py
@@ -13,3 +13,4 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     firstName = db.Column(db.String(150))
+    notes = db.relationship('Note')
