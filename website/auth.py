2e7f06da9c1568ee15e4cfa527e8f6a5512161a3
Nicholas <phamn1000@gmail.com>
2023-08-15 16:31:55 -0400

    The file now uses the user class in the file, creates a new user, commits the new user into the database and ending with a redirect to the home page.

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint("auth", __name__)
 
@@ -29,6 +32,19 @@ def register():
         elif len(password1) < 7:
             flash('Password Must be at least 7 Characters', category='error')
         else:
+
+            # Class Function for created new user to add to Database
+            new_user = User(email=email,
+                            firstName=firstName,
+                            password= generate_password_hash(password1, method='sha256'))
+            
+            # Adding and Committing the New User into Database
+            db.session.add(new_user)
+            db.session.commit()
+            
             flash('Account Created', category='success')
+
+            # Redirect user to the Home Page
+            return redirect(url_for('views.home'))
 
     return render_template("register.html")
