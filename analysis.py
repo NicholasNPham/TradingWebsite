b569856b6068c4fd72978df76aa651b0252c822c
Nicholas <phamn1000@gmail.com>
2023-08-16 22:58:30 -0400

    Added if, elif statement to detect if want summary or specified indicator.

--- analysis.py
+++ analysis.py
@@ -1,6 +1,6 @@
 from tradingview_ta import TA_Handler, Interval, Exchange
 
-def analysisForm(symbol, screener, exchange, interval):
+def analysisForm(symbol, screener, exchange, interval, analysisType):
 
     graph = TA_Handler(
         symbol = symbol,
@@ -8,4 +8,15 @@ def analysisForm(symbol, screener, exchange, interval):
         exchange = exchange,
         interval = interval)
 
-    return graph.get_analysis().summary
+    if analysisType == 'Summary':
+        return graph.get_analysis().summary
+    elif analysisType == 'Open':
+        return graph.get_analysis().indicators['open']
+    elif analysisType == 'Close':
+        return graph.get_analysis().indicators['close']
+    elif analysisType == 'Momentum':
+        return graph.get_analysis().indicators['Mom']
+    elif analysisType == 'RSI':
+        return graph.get_analysis().indicators['RSI']
+    elif analysisType == 'MACD':
+        return graph.get_analysis().indicators['MACD.macd']
