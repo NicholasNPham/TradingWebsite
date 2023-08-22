f8df738859dcfd0cc857af7591c00e5ad8ac036e
Nicholas <phamn1000@gmail.com>
2023-08-21 20:52:16 -0400

    Now only shown page after logging in.

--- website/views.py
+++ website/views.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template, request, flash
 from analysis import analysisForm
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
+@login_required
 def home():
     return render_template('home.html')
 
