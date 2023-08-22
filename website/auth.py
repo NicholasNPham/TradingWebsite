c23e2bcd798b282b9fd4c4b726796adeec0b5b1d
Nicholas <phamn1000@gmail.com>
2023-08-21 20:48:31 -0400

    Imported flask login module. Set the website to remember, login the user, and hold data aslong as the website is active in current.

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint("auth", __name__)
 
@@ -15,6 +16,7 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged In Successfully!', category='success')
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect Email or Password, Try Again', category='error')
@@ -56,6 +58,7 @@ def register():
             # Adding and Committing the New User into Database
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             
             flash('Account Created', category='success')
 
