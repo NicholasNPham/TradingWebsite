3951d5ca36bf3f7d14208f40ad6347e489e1b7e0
Nicholas <phamn1000@gmail.com>
2023-08-29 15:52:19 -0400

    Added to allow adding notes to webpage. Need to delete notes next week.

--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, request, flash
 from analysis import analysisForm
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
 
 # Pandas Module Reading Excel Sheet
 import pandas as pd
@@ -12,9 +14,22 @@ tickers = excelData['Symbol'].tolist()
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+             new_note = Note(data=note, user_id=current_user.id)
+             db.session.add(new_note)
+             db.session.commit()
+             flash('Note Added', category='success')
+        
+
+
     return render_template('home.html', user=current_user)
 
 @views.route('/info')
