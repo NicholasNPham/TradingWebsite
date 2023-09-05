980034eefa4cdb9fff8b2c5fe4035263f0bc1a49
Nicholas <phamn1000@gmail.com>
2023-09-05 00:25:08 -0400

    Added function to delete note from user if current user is holding the current user notes and none other.

--- website/views.py
+++ website/views.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, jsonify
 from analysis import analysisForm
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
+import json
 
 # Pandas Module Reading Excel Sheet
 import pandas as pd
@@ -33,6 +34,18 @@ def home():
 @views.route('/info')
 def info():
     return render_template('info.html', user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+            
+    return jsonify({})
 
 @views.route('/analysis', methods=['GET', 'POST'])
 def analysis():
